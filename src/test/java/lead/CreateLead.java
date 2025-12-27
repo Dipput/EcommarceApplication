8cac831c49dd0f55ce98663f8c80ac43551e1012
Dell <Dell@DESKTOP-5R28C3M>
2025-12-27 12:51:21 +0530

    resolved

--- src/test/java/lead/CreateLead.java
+++ src/test/java/lead/CreateLead.java
@@ -5,6 +5,7 @@ public class CreateLead {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("lead created");
+System.out.println("updated");
 
 	}
 
